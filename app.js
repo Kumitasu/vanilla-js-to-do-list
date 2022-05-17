c005f2af1f5cde614d90b4c35c5bbdf150cbfe89
Kumitasu <grzybolym@gmail.com>
2022-05-17 13:40:49 +0200

    adding first js functions

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+
+todoButton.addEventListener('click', addTodo);
+
+function addTodo(event) {
+    // Preventing from submitting
+    event.preventDefault();
+    // Todo Div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    //Creat Li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //Complete Button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-button");
+    todoDiv.appendChild(completedButton);
+    //Delete Button
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+    deleteButton.classList.add("complete-button");
+    todoDiv.appendChild(deleteButton);
+    //Append to list
+    todoList.appendChild(todoDiv);
+
+
+}
