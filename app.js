f0f31665e1f4af63cde931080d79bece7b88bf66
Kumitasu <grzybolym@gmail.com>
2022-05-19 13:16:12 +0200

    js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption =document.querySelector('.filter-todo');
 
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
 
 function addTodo(event) {
     // Preventing from submitting
@@ -34,9 +36,36 @@ function addTodo(event) {
 }
 
 function deleteCheck(e){
-    const item = e.target.checked;
+    const item = e.target;
     //Delete TodoDiv
     if(item.classList[0] === "delete-button"){
-        to
+        const todo = item.parentElement;
+        todo.classList.add("fall");
+        todo.addEventListener("transitionend",function(){
+            todo.remove();
+        })
+        
     }
+
+    if(item.classList[0] === "complete-button"){
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
+}
+
+function filterTodo(e){
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo){
+        switch(e.target.value){
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if(todo.classList.contains("completed")){
+                    todo.style.display = "flex";
+                }else{
+                    todo.style.display = "none";
+                }    
+        }
+    });
 }
